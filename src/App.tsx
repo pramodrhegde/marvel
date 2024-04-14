19abf1c674c555424430a1acc5d1f610fcc97d68
pramodrhegde <pramod.rh@gmail.com>
2024-04-14 14:43:34 +0530

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./main.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="content">
+        <p>Hello React!</p>
+      </div>
+      <div className="footer">
+        Use the Shell to install new packages.
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
