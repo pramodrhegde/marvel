c21dfa3d643ccdaf78ac5ddc743d3d2e9c51a98e
pramodrhegde <pramod.rh@gmail.com>
2024-04-16 14:14:35 +0530

    fix: build update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
