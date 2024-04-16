b005374477ee7aabbac253e0f852fb11ae87643e
pramodrhegde <pramod.rh@gmail.com>
2024-04-16 14:51:28 +0530

    Updates

--- bundle.js
+++ bundle.js
@@ -631,7 +631,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		__webpack_require__.p = "/";
+/******/ 		__webpack_require__.p = "/marvel/";
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/nonce */
