f270769c6d3d5fa5c76a78f8d10a1ef2dca054b9
pramodrhegde <pramod.rh@gmail.com>
2024-04-16 14:00:30 +0530

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-# marvel
-Marvel comics browser
-
-# running project from local
-
-`npm start` should spin up the local server and open the react app at `localhost:8000`
--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    moduleNameMapper: {
-      '\\.css$': 'identity-obj-proxy',
-    },
-  };
-  
